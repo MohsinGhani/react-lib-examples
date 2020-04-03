3d9e690857b8ee8a3711e4acef48e2146a44c9f1
Mohsin Ghani <mohsinghani@Mohsins-MacBook-Pro.local>
2020-04-03 16:08:08 +0500

    create react-cropper and react-player example

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ReactPlayerExample from './Components/ReactPlayerExample'
+import CropperDemo from './Components/CropperDemo'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    selectedFile: null
+  }
+
+  onChangeHandler = event => {
+    const { files } = event.target
+    let reader = new FileReader()
+    reader.readAsDataURL(files[0])
+
+    reader.onload = (e) => {
+      this.setState({
+        selectedFile: e.target.result
+      })
+    }
+  }
+
+  render() {
+    const { selectedFile } = this.state
+    return (
+      <div>
+        <hr />
+        <CropperDemo />
+        <hr />
+        <ReactPlayerExample />
+      </div>
+    )
+  }
 }
 
 export default App;
